e2f737b0adf7d5026fc5c246ca64efcbd377c7a4
Ellisande <justin.dragos@gmail.com>
2015-04-28 22:20:44 -0700

    Changed to import the modules for the commands and use an observer pattern on them.

--- index.js
+++ index.js
@@ -1,52 +1,25 @@
 #!/usr/bin/env node
-var fs = require('fs');
-var http = require('http');
-var request = require('superagent');
-var endpoints = require('./endpoints');
-var appDetails = require('./package');
-var params = require('yargs')
+var yargs = require('yargs')
   .usage('Usage: epm <command> [options]')
   .command('consume', 'Consume a webservice from the registry')
   .command('cache', 'Grab a local copy of your endpoints and their api keys')
   .command('provide', 'List your service as a provider')
-  .demand(2)
-  .argv;
+  .command('bump', 'Bump the current provider version and list it as consumable')
+  .alias('bump-version')
+  .demand(1)
 
+var command = yargs.argv._[0];
 
-var appDetails = require('./package.json');
-console.log(params);
-var paramKey = params._[0];
-if(paramKey == 'consume'){
-  var serviceToConsume = params._[1];
-  var versionToConsume = params.v || 'x.x.x';
-  console.log("I'm consuming!");
-  request
-    // .get('http://localhost:3000/some-url')
-    .post('http://localhost:3000/contract/add')
-    .send({
-      "provider": {
-          "name": serviceToConsume,
-          "version": versionToConsume
-      },
-      "consumer": {
-          "version": appDetails.version,
-          "name": appDetails.name
-      }
-    })
-    .end(function(err, res){
-      console.log('I hit the service');
-      var outputFilename = 'endpoints.json';
-      Object.keys(res.body).forEach(function(key){
-        endpoints[key] = res.body[key];
-      })
-      fs.writeFile(outputFilename, JSON.stringify(endpoints, null, 4));
-    })
-}
+var commands = [];
+commands.push(require('./commands/consume'));
+commands.push(require('./commands/cache'));
+commands.push(require('./commands/remove'));
+commands.push(require('./commands/provide'));
+commands.push(require('./commands/bump'));
 
-if(paramKey == 'cache'){
-  console.log("I'm caching!");
-}
-
-if(paramKey == 'provide'){
-  console.log("I'm providing!");
-}
+commands.some(function(currentCommand){
+  if(currentCommand.accepts(command)) {
+    currentCommand.execute();
+    return true;
+  }
+});
